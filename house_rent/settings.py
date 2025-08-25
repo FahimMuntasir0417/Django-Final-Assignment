6b65a2145a95e5293f6109025fd24b05081d9c99
Fahim Muntasir <FahimMuntasir0417@users.noreply.github.com>
2025-08-25 21:40:47 +0600

    Add collected static files

--- house_rent/settings.py
+++ house_rent/settings.py
@@ -77,9 +77,10 @@ CLOUDINARY_STORAGE = {
 
 MIDDLEWARE = [
     
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    
     
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -176,7 +177,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
